90561e002974ee826a8d42a2cf1e4c465f42d8e1
Diana Nguyen <diananguyen@Dianas-MBP-2.attlocal.net>
2022-10-23 22:24:57 -0700

    added clear button on js and high score list

--- Assets/highScores.js
+++ Assets/highScores.js
@@ -1,2 +1,23 @@
 var highScores = $(".high-scores"); //target location for high scores
 
+function showScores() {
+    var allScores = localStorage.getItem("allScores");
+    allScores = JSON.parse(allScores);
+
+if (allScores !== null) {
+    for (var i = 0; i <allScores.length; i++) {
+        var createLi = $("<li>"); 
+        highScores.append(createLi);
+    }
+    var clearButton = $("<button"); //creates a clear button
+    clearButton.attr("id", "clearButton");
+    clearButton.text("CLEAR ALL");
+    highScores.append(clearButton);
+    var buttonToClear = $("clearButton");
+    buttonToClear.on("click", function() {
+        highScores.text("");
+        buttonToClear.hide ();
+        localStorage.clear();
+    })
+};
+}
